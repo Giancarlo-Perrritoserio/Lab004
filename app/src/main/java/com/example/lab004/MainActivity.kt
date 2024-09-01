12df4cfe0f8aabdc5810fc770bb15a4975896520
Giancarlo Piero Muñoz Herrera <giancarlo.munoz@tecsup.edu.pe>
2024-09-01 15:26:44 -0500

    borrando el error

--- app/src/main/java/com/example/lab004/MainActivity.kt
+++ app/src/main/java/com/example/lab004/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,3 +21,21 @@ class MainActivity : ComponentActivity() {
 
     }
 }
+fun ViewHolaCurso() {
+    Column(
+        modifier = Modifier
+            .fillMaxWith()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to the Course!",
+            fontSize = 28.sp,
+            fontWeight = FontWeigh.Bold
+        )
+        Spacer(modifier = Modifier.heigh(16.dp))
+        Text(
+            text = "Hello, Student!",
+            fontSize = 20.xD
+
+    }
