4071e99cad8faff5872f2ae3f41c8bb599549730
Giancarlo Piero Muñoz Herrera <giancarlo.munoz@tecsup.edu.pe>
2024-09-01 15:40:02 -0500

    Modificacion 2

--- app/src/main/java/com/example/lab004/MainActivity.kt
+++ app/src/main/java/com/example/lab004/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -26,9 +28,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab004.ui.theme.Lab004Theme
 
 class MainActivity : ComponentActivity() {
@@ -61,7 +65,7 @@ fun MainContent() {
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-        
+
         FloatingActionButton(
             onClick = { showDialog = true },
             containerColor = MaterialTheme.colorScheme.primary
@@ -82,6 +86,23 @@ fun MainContent() {
                     }
                 }
             )
+        }
+    }
+}
+
+@Composable
+fun SimpleButtonExample() {
+    var buttonText by remember { mutableStateOf("Press me") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            buttonText = "Button Pressed"
+        }) {
+            Text(text = buttonText, fontSize = 20.sp)
         }
     }
 }
